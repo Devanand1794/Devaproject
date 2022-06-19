5c146ef7aa5dfd6c6c3cfcb285335601e895bde6
Devanand1794 <devanand1794@gmail.com>
2022-06-19 14:30:41 +0530

    compete

--- src/test/java/org/test/ClassA.java
+++ src/test/java/org/test/ClassA.java
@@ -22,7 +22,7 @@ public class ClassA {
 		System.out.println("Test A3 is completed");
 		System.out.println("Test A4 is completed");
 		System.out.println("Test A5 is completed");
-		System.out.println("Test A 6 is completed");
+		System.out.println("Test A6 is completed");
 	}
 	
 }
